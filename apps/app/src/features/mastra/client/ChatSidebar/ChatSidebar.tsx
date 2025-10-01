92ca5f155684a3b498bb2e45a6711bca67c2539c
Shun Miyazawa <m0zurillex@gmail.com>
2025-10-01 08:11:25 +0000

    Use useChat()

--- apps/app/src/features/mastra/client/ChatSidebar/ChatSidebar.tsx
+++ apps/app/src/features/mastra/client/ChatSidebar/ChatSidebar.tsx
@@ -1,4 +1,6 @@
 
+import { useChat } from '@ai-sdk/react';
+import { DefaultChatTransport } from 'ai';
 import SimpleBar from 'simplebar-react';
 
 import styles from './ChatSidebar.module.scss';
@@ -6,6 +8,11 @@ import styles from './ChatSidebar.module.scss';
 const moduleClass = styles['grw-chat-sidebar'] ?? '';
 
 const ChatSidebarSubstance = (): JSX.Element => {
+  const { messages, sendMessage } = useChat({
+    transport: new DefaultChatTransport({ api: '/_api/v3/mastra/message' }),
+  });
+
+
   return (
     <div
       className={`position-fixed top-0 end-0 h-100 border-start bg-body shadow-sm overflow-hidden ${moduleClass}`}
@@ -14,6 +21,23 @@ const ChatSidebarSubstance = (): JSX.Element => {
         className="h-100"
         autoHide
       >
+
+        {/* 後で消す */}
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            sendMessage(
+              { role: 'user', parts: [{ type: 'text', text: 'こんにちは' }] },
+              { body: { aiAssistantId: '68ccfba032a1048fe5548d5d' } },
+            );
+          }}
+        >
+          Send Message
+        </button>
+        {/* ここまで */}
+
+
       </SimpleBar>
     </div>
   );
