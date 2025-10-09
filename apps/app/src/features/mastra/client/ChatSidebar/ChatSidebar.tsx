a9863a11ab75b218a81f802010e32f274b195f2f
Shun Miyazawa <m0zurillex@gmail.com>
2025-10-09 09:53:47 +0000

    Add a tailwind sample (2)

--- apps/app/src/features/mastra/client/ChatSidebar/ChatSidebar.tsx
+++ apps/app/src/features/mastra/client/ChatSidebar/ChatSidebar.tsx
@@ -31,8 +31,10 @@ const ChatSidebarSubstance = (): JSX.Element => {
           Send Message
         </button>
 
-        {/* eslint-disable-next-line max-len */}
-        <button type="button" className="tw:text-white tw:bg-gradient-to-r tw:from-purple-500 tw:to-pink-500 hover:tw:bg-gradient-to-l tw:focus:ring-4 tw:focus:outline-none tw:focus:ring-purple-200 dark:tw:focus:ring-purple-800 tw:font-medium tw:rounded-lg tw:text-sm tw:px-5 tw:py-2.5 tw:text-center tw:me-2 tw:mb-2">tailwind button</button>
+        {/* tailwind alert examples */}
+        <div className="tw:p-4 tw:mb-4 tw:text-sm tw:text-blue-800 tw:rounded-xl tw:bg-blue-50 tw:dark:bg-gray-800 tw:dark:text-blue-400" role="alert">
+          <span className="tw:font-medium ">Hello Tailwind CSS!</span>
+        </div>
         {/* ============================================ */}
 
       </SimpleBar>
