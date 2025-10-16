84c3c178cc1179a939f2f7c1ae98885c9cc1eb9f
arvid-e <arvid.edvinsson@gmail.com>
2025-10-16 10:53:02 +0000

    Start working on date utility service

---
+++ apps/app/src/server/routes/contributions.ts
@@ -0,0 +1,8 @@
+// !!
+// not sure where to put the utility functions
+// so put them here temporarely
+
+export const getCurrentWeekNumber = (date: Date) => {
+  const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+
+};
