72d73e2780c3b174e7f412a77c13f5c51d76e18a
arvid-e <arvid.edvinsson@gmail.com>
2025-10-20 10:49:51 +0000

    Change week number function to return week id instead

--- apps/app/src/server/routes/contributions.ts
+++ apps/app/src/server/routes/contributions.ts
@@ -3,22 +3,17 @@
 // so put them here temporarely
 
 
-export const getISOWeekNumber = (date: Date) => {
+export const getISOWeekId = (date: Date) => {
   const utcDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-
   const dayNum = utcDate.getUTCDay() || 7;
-
   utcDate.setUTCDate(utcDate.getUTCDate() + 4 - dayNum);
-
   const yearStart = new Date(Date.UTC(utcDate.getUTCFullYear(), 0, 1));
-
   const oneDayInMs = 86400000; // 1000 * 60 * 60 * 24
-
   const diffInMs = utcDate.getTime() - yearStart.getTime();
+  const weekNumber = Math.ceil(((diffInMs / oneDayInMs) + 1) / 7);
+  const weekId = `${utcDate.getFullYear}-W${weekNumber}`;
 
-  const weekNo = Math.ceil(((diffInMs / oneDayInMs) + 1) / 7);
-
-  return weekNo;
+  return weekId;
 };
 
 
