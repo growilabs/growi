27721647abc8462d15c3827df4a04f5adb789d57
Ryu Sato <ryu@weseek.co.jp>
2025-10-22 14:17:29 +0000

    fix: move accessTokenHeaderAuth to the correct position in security schemes

--- apps/app/bin/openapi/definition-apiv3.js
+++ apps/app/bin/openapi/definition-apiv3.js
@@ -24,8 +24,8 @@ module.exports = {
   security: [
     {
       bearer: [],
-      accessTokenHeaderAuth: [],
       accessTokenInQuery: [],
+      accessTokenHeaderAuth: [],
     },
   ],
   components: {
