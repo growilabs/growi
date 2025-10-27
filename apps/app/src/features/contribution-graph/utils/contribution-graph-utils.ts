253092a6e71d8810e11dee3027633e50ccb8b08b
arvid-e <arvid.edvinsson@gmail.com>
2025-10-27 09:34:59 +0000

    Simplify date util function using date-fns and make simple tests

---
+++ apps/app/src/features/contribution-graph/utils/contribution-graph-utils.ts
@@ -0,0 +1,20 @@
+import { differenceInDays, format, startOfWeek } from 'date-fns';
+
+/**
+ * Gets current week's ISO week ID, e.g 2025-W32
+ */
+export const getISOWeekId = (date: Date): string => {
+  return format(date, "RRRR-'W'II");
+};
+
+export const daysSinceLastUpdate = (
+    lastUpdateDate: Date,
+    currentDate: Date = new Date(),
+): number => {
+  const diffDays = differenceInDays(lastUpdateDate, currentDate);
+  return Math.max(0, diffDays);
+};
+
+export const getCurrentWeekStart = (date: Date = new Date()): Date => {
+  return startOfWeek(date, { weekStartsOn: 1 });
+};
