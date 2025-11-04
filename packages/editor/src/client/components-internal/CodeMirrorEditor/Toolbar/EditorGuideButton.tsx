b0cc01645e0ef90931be5df31653af4e2aef7362
satof3 <satof.design@gmail.com>
2025-11-04 05:08:12 +0000

    Disappear button shorter than lg

--- packages/editor/src/client/components-internal/CodeMirrorEditor/Toolbar/EditorGuideButton.tsx
+++ packages/editor/src/client/components-internal/CodeMirrorEditor/Toolbar/EditorGuideButton.tsx
@@ -12,7 +12,7 @@ export const EditorGuideButton = (): JSX.Element => {
   return (
     <button
       type="button"
-      className="btn btn-toolbar-button"
+      className="btn btn-toolbar-button d-none d-lg-block"
       onClick={onClickEditorGuideButton}
     >
       <span className="growi-custom-icons fs-6">editor_guide</span>
