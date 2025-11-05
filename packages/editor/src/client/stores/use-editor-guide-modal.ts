446d255a5978047840ea2a4bddb138ca5bea17b6
satof3 <satof.design@gmail.com>
2025-11-05 09:51:36 +0000

    Remove export

--- packages/editor/src/client/stores/use-editor-guide-modal.ts
+++ packages/editor/src/client/stores/use-editor-guide-modal.ts
@@ -1,7 +1,7 @@
 import { useSWRStatic } from '@growi/core/dist/swr';
 import type { SWRResponse } from 'swr';
 
-export type EditorGuideModalStatus = {
+type EditorGuideModalStatus = {
   isOpened: boolean;
 };
 
