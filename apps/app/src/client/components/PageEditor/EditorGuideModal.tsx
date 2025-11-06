43f62a95f99f836e507961d785cb489165a8130b
satof3 <satof.design@gmail.com>
2025-11-06 07:45:38 +0000

    Apply FB

--- apps/app/src/client/components/PageEditor/EditorGuideModal.tsx
+++ apps/app/src/client/components/PageEditor/EditorGuideModal.tsx
@@ -16,22 +16,15 @@ export const EditorGuideModal = (props: Props): JSX.Element => {
     <>
       {/* Editor Guide Modal Overlay */}
       <div
-        className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"
-        style={{
-          zIndex: 1040,
-        }}
+        className="position-absolute w-100 h-100 modal-backdrop fade show z-2"
         onClick={onClose}
       />
 
       {/* Editor Guide Modal */}
       <div
-        className="position-fixed top-0 bottom-0 start-50 end-0 d-flex align-items-center justify-content-center"
-        style={{
-          zIndex: 1050,
-          pointerEvents: 'none',
-        }}
+        className="position-fixed top-0 bottom-0 start-50 end-0 d-flex align-items-center justify-content-center z-3 pe-none"
       >
-        <div className="px-3" style={{ pointerEvents: 'auto' }}>
+        <div className="px-3">
           <div className="card shadow-lg">
             <div className="card-header d-flex justify-content-between align-items-center">
               <h5 className="mb-0">Editor Guide</h5>
