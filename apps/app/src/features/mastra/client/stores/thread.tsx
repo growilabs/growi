73365651c1dbb841b045ddfad3284a32aeab34b6
Shun Miyazawa <m0zurillex@gmail.com>
2025-11-11 03:48:04 +0000

    Create useSWRINFxRecentThreads

---
+++ apps/app/src/features/mastra/client/stores/thread.tsx
@@ -0,0 +1,40 @@
+import type { PaginationInfo, StorageThreadType } from '@mastra/core';
+import type { SWRConfiguration } from 'swr';
+import type { SWRInfiniteResponse } from 'swr/infinite';
+import useSWRInfinite from 'swr/infinite';
+
+import { apiv3Get } from '~/client/util/apiv3-client';
+
+type PaginatedThread = PaginationInfo & { threads: StorageThreadType[] };
+
+const getRecentThreadsKey = (
+  pageIndex: number,
+  previousPageData: PaginatedThread | null,
+): [string, number, number] | null => {
+  if (previousPageData && !previousPageData.hasMore) {
+    return null;
+  }
+
+  const PER_PAGE = 20;
+  const page = pageIndex + 1;
+
+  return ['/mastra/threads', page, PER_PAGE];
+};
+
+export const useSWRINFxRecentThreads = (
+  config?: SWRConfiguration,
+): SWRInfiniteResponse<PaginatedThread, Error> => {
+  return useSWRInfinite(
+    (pageIndex, previousPageData) =>
+      getRecentThreadsKey(pageIndex, previousPageData),
+    ([endpoint, page, perPage]) =>
+      apiv3Get<PaginatedThread>(endpoint, { page, perPage }).then(
+        (response) => response.data.threads,
+      ),
+    {
+      ...config,
+      revalidateFirstPage: false,
+      revalidateAll: true,
+    },
+  );
+};
