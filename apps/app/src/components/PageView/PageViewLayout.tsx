744272c7ca4d40a9fe74820c48b70da7b0643c97
Shun Miyazawa <m0zurillex@gmail.com>
2025-11-17 10:45:23 +0000

    Revert "for ci"
    This reverts commit 9c0d5224a4b87b3ff6ddb806dbd452e8c90f108c.

--- apps/app/src/components/PageView/PageViewLayout.tsx
+++ apps/app/src/components/PageView/PageViewLayout.tsx
@@ -1,7 +1,5 @@
 import type { JSX, ReactNode } from 'react';
 
-import { usePrintMode } from '~/client/services/use-print-mode';
-
 import styles from './PageViewLayout.module.scss';
 
 const pageViewLayoutClass = styles['page-view-layout'] ?? '';
