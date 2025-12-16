83aacc2c0532e864ce41acc6a7f40015364ca8a1
arvid-e <arvid.edvinsson@gmail.com>
2025-12-16 08:22:57 +0000

    Add prop for hiding user pages in ItemsTree

--- apps/app/src/client/components/ItemsTree/ItemsTree.tsx
+++ apps/app/src/client/components/ItemsTree/ItemsTree.tsx
@@ -36,6 +36,7 @@ type ItemsTreeProps = {
   isEnableActions: boolean
   isReadOnlyUser: boolean
   isWipPageShown?: boolean
+  isHidingUserPages: boolean
   targetPath: string
   targetPathOrId?: string,
   CustomTreeItem: React.FunctionComponent<TreeItemProps>
