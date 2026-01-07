cd0e31cdee5f985543a009b1e1e45ee35c5dd9ca
Yuki Takei <yuki@weseek.co.jp>
2026-01-07 11:35:56 +0000

    remove eslint-disable-next-line react-hooks/rules-of-hooks lines

--- apps/app/src/client/components/Sidebar/Sidebar.tsx
+++ apps/app/src/client/components/Sidebar/Sidebar.tsx
@@ -51,7 +51,6 @@ const ResizableArea = withLoadingProps<ResizableAreaProps>((useLoadingProps) =>
   dynamic(() => import('./ResizableArea').then((mod) => mod.ResizableArea), {
     ssr: false,
     loading: () => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
       const { children, ...rest } = useLoadingProps();
       return (
         <ResizableAreaFallback {...rest}>{children}</ResizableAreaFallback>
