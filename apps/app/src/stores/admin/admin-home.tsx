737e37eaee8de39e7b2d3c2f101f8466430d7641
Yuki Takei <yuki@weseek.co.jp>
2026-01-08 11:43:05 +0000

    add IResAdminHome type and update useSWRxAdminHome to use it

--- apps/app/src/stores/admin/admin-home.tsx
+++ apps/app/src/stores/admin/admin-home.tsx
@@ -2,18 +2,9 @@ import type { SWRResponse } from 'swr';
 import useSWR from 'swr';
 
 import { apiv3Get } from '~/client/util/apiv3-client';
+import type { IResAdminHome } from '~/interfaces/res/admin/admin-home';
 
-export type AdminHomeData = {
-  growiVersion: string;
-  nodeVersion: string;
-  npmVersion: string;
-  pnpmVersion: string;
-  envVars: Record<string, string>;
-  isV5Compatible: boolean;
-  isMaintenanceMode: boolean;
-};
-
-export const useSWRxAdminHome = (): SWRResponse<AdminHomeData, Error> => {
+export const useSWRxAdminHome = (): SWRResponse<IResAdminHome, Error> => {
   return useSWR('/admin-home/', (endpoint) =>
     apiv3Get(endpoint).then((response) => {
       return response.data.adminHomeParams;
