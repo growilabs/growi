cda8a1349fa6da7c9e01579e6d1c8530c302825b
arvid-e <arvid.edvinsson@gmail.com>
2026-01-27 07:18:02 +0000

    Create function for setting new contribution cache

--- apps/app/src/server/service/contribution-graph/cache-data-service.ts
+++ apps/app/src/server/service/contribution-graph/cache-data-service.ts
@@ -15,6 +15,7 @@ export async function getContributionCache(
     const contributionCache = await ContributionCache.findOne({
       userId,
     }).exec();
+
     return contributionCache;
   } catch (_error) {
     throw new Error(
@@ -33,3 +34,38 @@ export function cacheIsFresh(cache: ContributionGraphDocument | null): boolean {
 
   return true;
 }
+
+export function setContributionCache(
+  userId: string,
+  cache: ContributionGraphDocument,
+) {
+  try {
+    if (!userId || !cache) {
+      throw new Error(
+        'UserId and new contribution cache are required to update contribution cache',
+      );
+    }
+
+    const updatedCache = ContributionCache.findOneAndUpdate(
+      { userId },
+      {
+        $set: {
+          currentWeekData: cache.currentWeekData,
+          permanentWeeks: cache.permanentWeeks,
+          lastUpdated: cache.lastUpdated,
+        },
+      },
+      {
+        new: true,
+        upsert: true,
+        runValidators: true,
+      },
+    ).exec();
+
+    return updatedCache;
+  } catch (_error) {
+    throw new Error('Internal Server Error: Could not set contribution cache.');
+  }
+}
+
+export function rotatePermanentWeeks() {}
