0815545bbe4f77fd67c272860c562130a581cf21
arvid-e <arvid.edvinsson@gmail.com>
2026-01-27 06:51:55 +0000

    Create cache functions for getting cache and checking if fresh

--- apps/app/src/server/service/contribution-graph/cache-data-service.ts
+++ apps/app/src/server/service/contribution-graph/cache-data-service.ts
@@ -1,1 +1,35 @@
-export function getContributionCache() {}
+import {
+  ContributionCache,
+  type ContributionGraphDocument,
+} from './models/contribution-cache-model';
+import { getUTCMidnightToday } from './utils/contribution-graph-utils';
+
+export async function getContributionCache(
+  userId: string,
+): Promise<ContributionGraphDocument | null> {
+  try {
+    if (!userId) {
+      throw new Error('UserId is required to fetch contribution cache');
+    }
+
+    const contributionCache = await ContributionCache.findOne({
+      userId,
+    }).exec();
+    return contributionCache;
+  } catch (error) {
+    throw new Error(
+      'Internal Server Error: Could not retrieve contribution data',
+    );
+  }
+}
+
+export function cacheIsFresh(cache: ContributionGraphDocument | null): boolean {
+  if (!cache || !cache.lastUpdated) return false;
+
+  const lastUpdatedDate = new Date(cache.lastUpdated);
+  const todaysDate = getUTCMidnightToday();
+
+  if (lastUpdatedDate <= todaysDate) return false;
+
+  return true;
+}
