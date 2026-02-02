cc3a8488ec7c97a6c52af672c487df5463f49eef
arvid-e <arvid.edvinsson@gmail.com>
2026-02-02 04:32:10 +0000

    Move all contribution graph files to src/features/

--- apps/app/src/features/contribution-graph/services/cache-data-service.spec.ts
+++ apps/app/src/features/contribution-graph/services/cache-data-service.spec.ts
@@ -2,14 +2,14 @@ import { MongoMemoryServer } from 'mongodb-memory-server-core';
 import mongoose from 'mongoose';
 import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest';
 
+import { ContributionCache } from '../models/contribution-cache-model';
+import { getUTCMidnightToday } from '../utils/contribution-graph-utils';
 import {
   cacheIsFresh,
   getContributionCache,
   type SetContributionCachePayload,
   updateContributionCache,
 } from './cache-data-service';
-import { ContributionCache } from './models/contribution-cache-model';
-import { getUTCMidnightToday } from './utils/contribution-graph-utils';
 
 const createMockId = () => new mongoose.Types.ObjectId().toString();
 
