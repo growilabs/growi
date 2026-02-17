b95c44fee93719c2903e23fc97a76c857789088c
arvid-e <arvid.edvinsson@gmail.com>
2026-02-17 05:43:19 +0000

    Test and remove user prefixes using regex

--- apps/app/src/features/search/utils/disable-user-pages.ts
+++ apps/app/src/features/search/utils/disable-user-pages.ts
@@ -1,8 +1,10 @@
 import type { QueryTerms } from '~/server/interfaces/search';
 
 export function excludeUserPagesFromQuery(terms: QueryTerms): void {
-  terms.prefix = terms.prefix.filter((p) => !p.startsWith('/user'));
-  terms.not_prefix = terms.not_prefix.filter((p) => !p.startsWith('/user'));
+  const userRegex: RegExp = /^\/user($|\/)/;
+
+  terms.prefix = terms.prefix.filter((p) => !userRegex.test(p));
+  terms.not_prefix = terms.not_prefix.filter((p) => !userRegex.test(p));
 
   terms.not_prefix.push('/user');
 }
