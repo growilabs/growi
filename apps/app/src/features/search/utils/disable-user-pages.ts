2074dade6ea355f7a41a2893957e441149088185
arvid-e <arvid.edvinsson@gmail.com>
2026-02-25 07:49:36 +0000

    Update page exclusion function to not match double slashes

--- apps/app/src/features/search/utils/disable-user-pages.ts
+++ apps/app/src/features/search/utils/disable-user-pages.ts
@@ -1,7 +1,7 @@
 import type { QueryTerms } from '~/server/interfaces/search';
 
 export function excludeUserPagesFromQuery(terms: QueryTerms): void {
-  const userRegex: RegExp = /^\/user($|\/)/;
+  const userRegex: RegExp = /^\/user($|\/(?!\/))/;
 
   terms.prefix = terms.prefix.filter((p) => !userRegex.test(p));
   terms.not_prefix = terms.not_prefix.filter((p) => !userRegex.test(p));
