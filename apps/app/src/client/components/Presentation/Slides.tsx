74c18da80d41fc3d1ae2f8dd5d78d6de791fe117
Yuki Takei <yuki@weseek.co.jp>
2026-03-05 11:41:18 +0000

    dump reveal.css to dist/style.css

--- apps/app/src/client/components/Presentation/Slides.tsx
+++ apps/app/src/client/components/Presentation/Slides.tsx
@@ -4,8 +4,6 @@ import {
   Slides as SlidesSubstance,
 } from '@growi/presentation/dist/client';
 
-import './Presentation.vendor-styles.prebuilt';
-
 export const Slides = (props: SlidesProps): JSX.Element => {
   return <SlidesSubstance {...props} />;
 };
