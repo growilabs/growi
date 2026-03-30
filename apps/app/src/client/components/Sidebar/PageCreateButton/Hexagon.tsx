77ef53178addaf68e26bc7dbdda873cd3e426ade
yuyaiwahori <yuya-i@weseek.co.jp>
2026-03-30 09:16:47 +0000

    FB 修正(六角形の形、hoverのサイズ)

--- apps/app/src/client/components/Sidebar/PageCreateButton/Hexagon.tsx
+++ apps/app/src/client/components/Sidebar/PageCreateButton/Hexagon.tsx
@@ -7,17 +7,16 @@ type Props = {
 export const Hexagon = React.memo(
   (props: Props): JSX.Element => (
     <svg
+      width="41"
+      height="36"
+      viewBox="0 0 41 36"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 27.691 23.999"
-      height="36px"
       className={props.className}
     >
       <title>Create</title>
       <g className="background" transform="translate(0 0)">
-        <path
-          d="M19.768,0.5 L25.691,10.5 A3,3 0 0,1 25.691,13.5 L19.768,23.5 A2,2 0 0,1 17.768,24 L7.923,24 A2,2 0 0,1 5.923,23.5 L0,13.5 A3,3 0 0,1 0,10.5 L5.923,0.5 A2,2 0 0,1 7.923,0 L17.768,0 A2,2 0 0,1 19.768,0.5 Z"
-          transform="translate(0)"
-        ></path>
+        <path d="M28.5717 0C29.9943 0 31.3099 0.755645 32.0268 1.98452L40.1934 15.9845C40.92 17.23 40.92 18.77 40.1934 20.0155L32.0268 34.0155C31.3099 35.2444 29.9943 36 28.5717 36H12.1666C10.744 36 9.42835 35.2444 8.71151 34.0155L0.544839 20.0155C-0.181674 18.77 -0.181673 17.23 0.54484 15.9845L8.71151 1.98451C9.42835 0.755643 10.744 0 12.1666 0H28.5717Z" />
       </g>
     </svg>
   ),
