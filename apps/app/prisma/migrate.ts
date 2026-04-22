067441bb50ca822477502ea90cbc395a6ed1cd72
mizozobu <hamc.m.so.j12@gmail.com>
2026-04-22 15:05:16 +0000

    style(@growi/app): auto fix

--- apps/app/prisma/migrate.ts
+++ apps/app/prisma/migrate.ts
@@ -8,13 +8,16 @@ import { resolve } from 'node:path';
 import { MongoClient } from 'mongodb';
 import { MongoDBStorage, Umzug } from 'umzug';
 
-
 (async () => {
   const url = process.env.MONGO_URI;
   if (url === undefined) {
     throw new Error('MONGO_URI is required');
   }
-  const { prisma } = await import(process.env.NODE_ENV === 'production' ? '../dist/utils/prisma' : '../src/utils/prisma');
+  const { prisma } = await import(
+    process.env.NODE_ENV === 'production'
+      ? '../dist/utils/prisma'
+      : '../src/utils/prisma'
+  );
   const client = new MongoClient(url);
   await client.connect();
 
