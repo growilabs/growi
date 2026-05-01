2f94f5545da5846a958cefc6ab72cd813d46d0c7
Shun Miyazawa <34241526+miya@users.noreply.github.com>
2026-05-01 15:37:48 +0900

    Revert "always show page controls in drawer mode"

--- apps/app/src/client/components/Sidebar/PageTreeItem/PageTreeItem.tsx
+++ apps/app/src/client/components/Sidebar/PageTreeItem/PageTreeItem.tsx
@@ -22,7 +22,6 @@ import { useCurrentPagePath, useFetchCurrentPage } from '~/states/page';
 import { usePageDeleteModalActions } from '~/states/ui/modal/page-delete';
 import type { IPageForPageDuplicateModal } from '~/states/ui/modal/page-duplicate';
 import { usePageDuplicateModalActions } from '~/states/ui/modal/page-duplicate';
-import { useSidebarMode } from '~/states/ui/sidebar';
 import { mutateAllPageInfo } from '~/stores/page';
 import { mutatePageList, mutatePageTree } from '~/stores/page-listing';
 import { mutateSearching } from '~/stores/search';
@@ -133,8 +132,6 @@ export const PageTreeItem: FC<TreeItemProps> = ({
     ],
   );
 
-  const { isDrawerMode } = useSidebarMode();
-
   const { Control } = usePageItemControl();
 
   // Rename feature from usePageRename hook
@@ -182,15 +179,8 @@ export const PageTreeItem: FC<TreeItemProps> = ({
       onToggle={onToggle}
       onClickDuplicateMenuItem={onClickDuplicateMenuItem}
       onClickDeleteMenuItem={onClickDeleteMenuItem}
-      customEndComponents={
-        isDrawerMode() ? undefined : [CountBadgeForPageTreeItem]
-      }
-      customHoveredEndComponents={
-        isDrawerMode() ? undefined : [Control, CreateButton]
-      }
-      customPinnedEndComponents={
-        isDrawerMode() ? [Control, CreateButton] : undefined
-      }
+      customEndComponents={[CountBadgeForPageTreeItem]}
+      customHoveredEndComponents={[Control, CreateButton]}
       showAlternativeContent={isRenaming(item) || isCreatingPlaceholder(item)}
       customAlternativeComponents={[TreeNameInput]}
     />
