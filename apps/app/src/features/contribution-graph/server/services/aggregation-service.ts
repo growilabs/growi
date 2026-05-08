fdc397fd77e0c6d0ba9d7ee8d377503ad94d2baa
arvid-e <arvid.edvinsson@gmail.com>
2026-05-08 06:25:41 +0000

    Add back the aggregation service

---
+++ apps/app/src/features/contribution-graph/server/services/aggregation-service.ts
@@ -0,0 +1,58 @@
+import type { Aggregate, PipelineStage } from 'mongoose';
+import mongoose from 'mongoose';
+
+import Activity from '~/server/models/activity';
+
+import type { IContributionDay } from '../../interfaces/contribution-graph';
+import { ContributionGraphActions } from '../../interfaces/supported-actions';
+
+export interface PipelineParams {
+  userId: string;
+  startDate: Date;
+}
+
+export class ContributionAggregationService {
+  public runAggregationPipeline(
+    params: PipelineParams,
+  ): Aggregate<IContributionDay[]> {
+    const pipeline = this.buildPipeline(params);
+    const activityResults = Activity.aggregate(pipeline);
+
+    return activityResults;
+  }
+
+  public buildPipeline(params: PipelineParams): PipelineStage[] {
+    const { userId, startDate } = params;
+    const endDate = new Date();
+
+    return [
+      {
+        $match: {
+          user: new mongoose.Types.ObjectId(userId),
+          action: { $in: Object.values(ContributionGraphActions) },
+          createdAt: { $gte: startDate, $lte: endDate },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateTrunc: {
+              date: '$createdAt',
+              unit: 'day',
+              timezone: 'UTC',
+            },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: { $dateToString: { format: '%Y-%m-%d', date: '$_id' } },
+          count: '$count',
+        },
+      },
+      { $sort: { date: 1 } },
+    ];
+  }
+}
