b85097975b74f0f333984f12f9108d1fa394293c
Yuki Takei <yuki@weseek.co.jp>
2026-06-17 08:22:43 +0000

    convert orval configuration file to ESM

--- packages/pdf-converter-client/orval.config.ts
+++ packages/pdf-converter-client/orval.config.ts
@@ -1,6 +1,8 @@
-module.exports = {
+import { defineConfig } from 'orval';
+
+export default defineConfig({
   'client-library': {
     input: '../../apps/pdf-converter/specs/v3/docs/swagger.yaml',
     output: './src/index.ts',
   },
-};
+});
