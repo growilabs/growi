3250fe5d2440d52612adc9ad682af5b4030c786e
arvid-e <arvid.edvinsson@gmail.com>
2026-07-02 10:15:35 +0000

    Index field on schema

--- apps/app/src/features/backlinks/server/models/page-link.ts
+++ apps/app/src/features/backlinks/server/models/page-link.ts
@@ -3,25 +3,33 @@ import { Schema } from 'mongoose';
 import { getOrCreateModel } from '~/server/util/mongoose-utils';
 
 import type {
-  IPageLink,
   PageLinkDocument,
   PageLinkModel,
 } from '../../interfaces/page-link';
 
-const PageLinkSchema = new Schema<IPageLink>({
+const pageLinkSchema = new Schema<PageLinkDocument, PageLinkModel>({
   fromPage: {
     type: Schema.Types.ObjectId,
+    ref: 'Page',
+    required: true,
+    index: true,
   },
   toPath: {
     type: String,
+    required: true,
+    index: true,
   },
   toPage: {
     type: Schema.Types.ObjectId,
+    ref: 'Page',
     default: null,
+    index: true,
   },
 });
 
+pageLinkSchema.index({ fromPage: 1, toPath: 1 }, { unique: true });
+
 export default getOrCreateModel<PageLinkDocument, PageLinkModel>(
   'PageLink',
-  PageLinkSchema,
+  pageLinkSchema,
 );
