70937d3946e55c1c55a20a743c114defa12493ae
arvid-e <arvid.edvinsson@gmail.com>
2026-07-22 05:35:01 +0000

    Pass crowi through handler argument

--- apps/app/src/features/backlinks/server/routes/backlinks.ts
+++ apps/app/src/features/backlinks/server/routes/backlinks.ts
@@ -1,3 +1,4 @@
+import { SCOPE } from '@growi/core';
 import type { RequestHandler } from 'express';
 import type { ValidationChain } from 'express-validator';
 import { query } from 'express-validator';
@@ -5,6 +6,7 @@ import mongoose from 'mongoose';
 
 import type { CrowiRequest } from '~/interfaces/crowi-request';
 import type Crowi from '~/server/crowi';
+import { accessTokenParser } from '~/server/middlewares/access-token-parser';
 import { apiV3FormValidator } from '~/server/middlewares/apiv3-form-validator';
 import loginRequiredFactory from '~/server/middlewares/login-required';
 import type { ApiV3Response } from '~/server/routes/apiv3/interfaces/apiv3-response';
@@ -12,16 +14,16 @@ import loggerFactory from '~/utils/logger';
 
 const logger = loggerFactory('growi:routes:apiv3:backlink');
 
-export const getBacklinksHandler = (): RequestHandler => {
+export const getBacklinksHandler = (crowi: Crowi): RequestHandler => {
   return async (req: CrowiRequest, res: ApiV3Response) => {
-    const { toPageId } = req.query;
+    const { pageId } = req.query;
 
-    if (typeof toPageId !== 'string') {
-      return res.apiv3Err('toPageId must be a string', 400);
+    if (typeof pageId !== 'string') {
+      return res.apiv3Err('pageId must be a string', 400);
     }
     try {
-      const backlinks = await req.crowi.pageLinkService.findBacklinks(
-        new mongoose.Types.ObjectId(toPageId),
+      const backlinks = await crowi.pageLinkService.findBacklinks(
+        new mongoose.Types.ObjectId(pageId),
         req.user ?? null,
       );
       return res.apiv3({ backlinks });
@@ -33,20 +35,21 @@ export const getBacklinksHandler = (): RequestHandler => {
 };
 
 export const getBacklinksHandlerFactory = (crowi: Crowi): RequestHandler[] => {
-  const loginRequiredStrictly = loginRequiredFactory(crowi);
+  const loginRequired = loginRequiredFactory(crowi, true);
 
   const validator: ValidationChain[] = [
-    query('toPageId')
+    query('pageId')
       .notEmpty()
-      .withMessage('toPageId is required')
+      .withMessage('pageId is required')
       .isMongoId()
-      .withMessage('toPageId must be a MongoDB ID'),
+      .withMessage('pageId must be a MongoDB ID'),
   ];
 
   return [
-    loginRequiredStrictly,
+    accessTokenParser([SCOPE.READ.FEATURES.PAGE]),
+    loginRequired,
     ...validator,
     apiV3FormValidator,
-    getBacklinksHandler(),
+    getBacklinksHandler(crowi),
   ];
 };
